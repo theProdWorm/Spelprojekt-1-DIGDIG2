4aed5a28f0094049042d3a7d93ed90580cb8cc0c
theProdWorm <emil.rohlen@live.se>
2022-03-15 16:03:34 +0100

    slime theory mended

--- Assets/Scripts/Enemies/Slime.cs
+++ Assets/Scripts/Enemies/Slime.cs
@@ -19,13 +19,14 @@ public class Slime : Entity {
     protected override void Update ( ) {
         base.Update( );
 
-        if (lastSplit > 0) TryFuse( );
+        bool success = false;
+        if (lastSplit > 0 && size < Size.large) success = TryFuse( );
         else if (size == Size.large) TryConsume( );
-        else Nibble( );
+        else if (!success) Nibble( );
     }
 
     private void TryConsume ( ) {
-
+        
 
     }
 
@@ -33,11 +34,16 @@ public class Slime : Entity {
 
     }
 
-    private void TryFuse ( ) {
-
+    /// <summary>
+    /// Scans for slimes of the same size to fuse with, picking the center between them all as their meeting point
+    /// </summary>
+    private bool TryFuse ( ) {
         Slime[ ] slimesInRadius = (from slime in FindObjectsOfType<Slime>( )
                                    where Vector2.Distance(slime.transform.position, transform.position) <= partnerSearchRadius
+                                   && slime.size == size
                                    select slime).OrderByDescending(x => Vector2.Distance(x.transform.position, transform.position)).ToArray( );
+
+        if (slimesInRadius.Length < 3) return false;
 
         Vector2 meetingPoint = Vector2.zero;
 
@@ -49,33 +55,29 @@ public class Slime : Entity {
 
         transform.position = Vector2.MoveTowards(transform.position, meetingPoint, c_speed);
 
-
+        return true;
     }
 
     /// <summary>
     /// Instantiate smaller slimes, shooting out from the original one in random directions
     /// </summary>
     /// <returns></returns>
-    private bool Split ( ) {
-        if (size > 0) {
-            for (int i = 0; i < 3; i++) {
-                float angle = Random.Range(0, 2 * Mathf.PI);
-                float force = Random.Range(1.0f, 2.0f);
+    private void Split ( ) {
+        for (int i = 0; i < 3; i++) {
+            float angle = Random.Range(0, 2 * Mathf.PI);
+            float force = Random.Range(1.0f, 2.0f);
 
-                Vector2 forceVector = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * force;
+            Vector2 forceVector = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * force;
 
-                var newSlime = Instantiate(gameObject).GetComponent<Slime>( );
+            var newSlime = Instantiate(gameObject).GetComponent<Slime>( );
 
-                newSlime.size = size - 1;
-                newSlime.rb.AddForce(forceVector);
-            }
-
-            return true;
+            newSlime.size = size - 1;
+            newSlime.rb.AddForce(forceVector);
         }
-        else return false;
     }
 
     protected override void Die ( ) {
-        if (!Split( )) base.Die( );
+        if (size > 0) Split( );
+        else base.Die( );
     }
 }
