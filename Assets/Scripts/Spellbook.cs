aafa1544547f055ca94573b7877971dd91c7eeaf
theProdWorm <emil.rohlen@live.se>
2022-03-18 23:12:39 +0100

    spellbook returns correct spell

--- Assets/Scripts/Spellbook.cs
+++ Assets/Scripts/Spellbook.cs
@@ -5,43 +5,12 @@ using UnityEngine;
 public class Spellbook : MonoBehaviour
 {
     public Spell[ ] spells;
-    public Dictionary<Element[ ], Spell> spellCollection = new Dictionary<Element[], Spell>( );
-
-    private void Start ( ) {
-        for (int i = 0; i < spells.Length; i++) {
-            Element[ ] combo = spells[i].combo.OrderByDescending(e => e).Reverse( ).ToArray( );
-
-            string elArray = "[";
-            for (int j = 0; j < combo.Length; j++) {
-                elArray += combo[j];
-
-                if (j < combo.Length - 1)
-                    elArray += ", ";
-            }
-            elArray += "]";
-            print(elArray);
-
-            spellCollection[combo] = spells[i];
-        }
-    }
 
     public Spell GetSpell (List<Element> selectedElements) {
         Element[ ] combo = selectedElements.OrderByDescending(e => e).Reverse( ).ToArray( );
 
-        string n_combo = "spellCollection.ContainsKey(Element[";
-        for (int i = 0; i < combo.Length; i++) {
-            n_combo += (int) combo[i];
-
-            if (i < combo.Length - 1)
-                n_combo += ", ";
-        }
-        print(n_combo + "] = " + spellCollection.ContainsKey(combo) + ")");
-
-
-        if (!spellCollection.ContainsKey(combo)) {
-            return null;
-        }
-
-        return spellCollection[combo];
+        Spell _spell = spells.First(s => Enumerable.SequenceEqual(s.combo, combo));
+        
+        return _spell;
     }
 }
